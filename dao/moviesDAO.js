32dfa38b53e41608abf7b43ee9b7f214d3326890
dukewendo <dommct@yahoo.co.uk>
2024-07-29 08:17:14 +0100

    review DAO update

--- dao/moviesDAO.js
+++ dao/moviesDAO.js
@@ -1,49 +1,48 @@
 let movies;
 
 export default class MoviesDAO {
-	static async injectDB(conn) {
-		if (movies) {
-			return;
-		}
-		try {
-			movies = await conn.db(process.env.MOVIEREVIEWS_NS).collection("movies");
-		} catch (e) {
-			console.error(`unable to connect in MoviesDAO: ${e} `);
-		}
-	}
+  static async injectDB(conn) {
+    if (movies) {
+      return;
+    }
+    try {
+      movies = await conn.db(process.env.MOVIEREVIEWS_NS).collection("movies");
+    } catch (e) {
+      console.error(`unable to connect in MoviesDAO: ${e} `);
+    }
+  }
 
-	static async getMovies({
-		filters = null,
-		page = 0,
-		moviesPerPage = 20,
-	} = {}) {
+  static async getMovies({
+    filters = null,
+    page = 0,
+    moviesPerPage = 20,
+  } = {}) {
+    let query;
 
-		let query;
+    // Construct the query based on filters (if any)
+    if (filters) {
+      if ("title" in filters) {
+        query = { $text: { $search: filters["title"] } };
+      } else if ("rated" in filters) {
+        query = { rated: { $eq: filters["rated"] } };
+      }
+    }
 
-		// Construct the query based on filters (if any)
-		if (filters) {
-			if ("title" in filters) {
-				query = { $text: { $search: filters["title"] } };
-			} else if ("rated" in filters) {
-				query = { "rated": { $eq: filters["rated"] } };
-			}
-		}
+    let cursor;
 
-		let cursor;
-		
-		try {
-			cursor = await movies
-				.find(query)
-				.limit(moviesPerPage)
-				.skip(moviesPerPage * page);
+    try {
+      cursor = await movies
+        .find(query)
+        .limit(moviesPerPage)
+        .skip(moviesPerPage * page);
 
-			const moviesList = await cursor.toArray();
-			const totalNumMovies = await movies.countDocuments(query);
+      const moviesList = await cursor.toArray();
+      const totalNumMovies = await movies.countDocuments(query);
 
-			return { moviesList, totalNumMovies };
-		} catch (e) {
-			console.error(`Unable to issue find command", ${e}`);
-			return { moviesList: [], totalNumMovies: 0 };
-		}
-	}
+      return { moviesList, totalNumMovies };
+    } catch (e) {
+      console.error(`Unable to issue find command", ${e}`);
+      return { moviesList: [], totalNumMovies: 0 };
+    }
+  }
 }
