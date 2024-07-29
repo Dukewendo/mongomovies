7231e70c3607d5a883f3ca628c018e9f200f6f83
dukewendo <dommct@yahoo.co.uk>
2024-07-29 07:50:04 +0100

    reviews update

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 import app from "./server.js";
 import dotenv from "dotenv";
 import mongodb from "mongodb";
-import MoviesDAO from "./dao/moviesDAO.js"
-
-
+import MoviesDAO from "./dao/moviesDAO.js";
+import ReviewsDAO from "./doa/reviewsDAO.js";
 
 async function main() {
-	dotenv.config();
+  dotenv.config();
 
-	console.log("MONGODB URI:", process.env.MOVIEREVIEWS_DB_URI);
+  console.log("MONGODB URI:", process.env.MOVIEREVIEWS_DB_URI);
 
-	const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI);
+  const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI);
 
-	const port = process.env.PORT || 8000;
+  const port = process.env.PORT || 8000;
 
-	try {
-		await client.connect();
-		await MoviesDAO.injectDB(client)
+  try {
+    await client.connect();
+    await MoviesDAO.injectDB(client);
+    await ReviewsDAO.injectDB(client);
 
-		app.listen(port, () => {
-			console.log(`server is running on port: ${port}`);
-		});
-	} catch (e) {
-		console.error(e);
-		process.exit(1);
-	}
+    app.listen(port, () => {
+      console.log(`server is running on port: ${port}`);
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 }
 
 main().catch(console.error);
