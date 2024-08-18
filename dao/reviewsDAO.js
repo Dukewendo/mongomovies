cd98a35ffcacc49997dde6f8e7e7544cb7140ad4
dukewendo <dommct@yahoo.co.uk>
2024-08-18 12:33:44 +0100

    typeo in reviewsDAO fixed

--- dao/reviewsDAO.js
+++ dao/reviewsDAO.js
@@ -45,7 +45,7 @@ export default class ReviewsDAO {
     }
   }
 
-  static asyncdeleteReview(reviewId, userId) {
+  static async deleteReview(reviewId, userId) {
     try{
       const deleteResponse = await reviews.deleteOne({
         _id: ObjectId(reviewId),
