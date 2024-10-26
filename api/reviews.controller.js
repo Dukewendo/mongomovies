805b97487ecd3fd3d22d477ea4abc4832d98e54a
dukewendo <dommct@yahoo.co.uk>
2024-10-26 18:57:11 +0100

    update and delete review

--- api/reviews.controller.js
+++ api/reviews.controller.js
@@ -52,4 +52,15 @@ export default class ReviewsController {
       res.status(500).json({ error: e.message });
     }
   }
+
+  static async apiDeleteReview(req, res, next) {
+    try {
+      const reviewId = req.body.review_id;
+      const userId = req.body.user_id;
+      const ReviewResponse = await ReviewsDAO.deleteReview(reviewId, userId);
+      res.jon({ status: "success " });
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  }
 }
