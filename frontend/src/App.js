c636212fc2faf7900facc1132a367fe95dca342d
Dukewendo <dommct@yahoo.co.uk>
2024-12-09 19:16:27 +0000

    added conditional login for navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,12 +18,16 @@ function App() {
 		<Router>
 			<div className="App">
 				<Navbar bg="light" expand="lg">
-					<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+					<Navbar.Brand href="#home">Movie Reviews</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="mr-auto">
-							<Nav.Link href="#home">Home</Nav.Link>
-							<Nav.Link href="#link">Link</Nav.Link>
+							<Nav.Link>
+								<Link to={"/movies"}>Movies</Link>
+							</Nav.Link>
+							<Nav.Link>
+								{false ? <a>Logout User</a> : <Link to={"/login"}>Login</Link>}
+							</Nav.Link>
 						</Nav>
 					</Navbar.Collapse>
 				</Navbar>
