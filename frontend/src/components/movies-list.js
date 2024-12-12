9af1474f57db9ed1566611f668d5e2205887924f
Dukewendo <dommct@yahoo.co.uk>
2024-12-12 21:42:35 +0000

    movies-list setting state

--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import MovieDataService from "../services/movies";
+import { link } from "react-router-dom";
 
-function MoviesList() {
-	return <div className="App">Movies List</div>;
-}
-
-export default MoviesList;
+const MoviesList = (props) => {
+	const [movies, setMovies] = useState([]);
+	const [searchTitle, setSearchTitle] = useState("");
+	const [searchRating, setSearchRating] = useState("");
+	const [ratings, setRatings] = useState("");
+};
