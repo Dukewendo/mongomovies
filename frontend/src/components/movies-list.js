8277672721613f42ab3486de95ebe6241738bdd8
Dukewendo <dommct@yahoo.co.uk>
2024-12-12 21:57:01 +0000

    added useEffect hook, retrieveMovies and retrieveRatings

--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -1,10 +1,37 @@
 import React, { useState, useEffect } from "react";
 import MovieDataService from "../services/movies";
-import { link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MoviesList = (props) => {
 	const [movies, setMovies] = useState([]);
 	const [searchTitle, setSearchTitle] = useState("");
 	const [searchRating, setSearchRating] = useState("");
 	const [ratings, setRatings] = useState("");
+};
+
+useEffect(() => {
+	retrieveMovies();
+	retrieveRatings();
+}, []);
+
+const retrieveMovies = () => {
+	MovieDataService.getAll()
+		.then((response) => {
+			console.log(response.data);
+			setMovies(response.data.movies);
+		})
+		.catch((e) => {
+			console.log(e);
+		});
+};
+
+const retrieveRatings = () => {
+	MovieDataService.getRatings()
+		.then((response) => {
+			console.log(resonse.data);
+			setRatings(["All Ratings"].concat(response.data));
+		})
+		.catch((e) => {
+			console.log(e);
+		});
 };
