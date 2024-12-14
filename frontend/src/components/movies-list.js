0cf022f4c77036b8baea6899793b4db884d8bda4
Dukewendo <dommct@yahoo.co.uk>
2024-12-14 23:44:46 +0000

    importing form components for form

--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from "react";
 import MovieDataService from "../services/movies";
 import { Link } from "react-router-dom";
 
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
 const MoviesList = (props) => {
 	const [movies, setMovies] = useState([]);
 	const [searchTitle, setSearchTitle] = useState("");
