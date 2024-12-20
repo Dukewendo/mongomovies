84af110e3d97d55797b80f0055288338d894b25c
Dukewendo <dommct@yahoo.co.uk>
2024-12-20 22:25:19 +0000

    buf fixes and correcting spelling erros

--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -14,117 +14,117 @@ const MoviesList = (props) => {
 	const [searchTitle, setSearchTitle] = useState("");
 	const [searchRating, setSearchRating] = useState("");
 	const [ratings, setRatings] = useState("");
-};
 
-useEffect(() => {
-	retrieveMovies();
-	retrieveRatings();
-}, []);
-
-const retrieveMovies = () => {
-	MovieDataService.getAll()
-		.then((response) => {
-			console.log(response.data);
-			setMovies(response.data.movies);
-		})
-		.catch((e) => {
-			console.log(e);
-		});
-};
-
-const retrieveRatings = () => {
-	MovieDataService.getRatings()
-		.then((response) => {
-			console.log(resonse.data);
-			setRatings(["All Ratings"].concat(response.data));
-		})
-		.catch((e) => {
-			console.log(e);
-		});
-};
-
-const onChangeSearchTitle = (e) => {
-	const searchTitle = e.target.value;
-	setSearchTitle(searchTitle);
-};
-
-const onChangeSearchRating = (e) => {
-	const searchRating = e.target.valuesetSearchRating(searchRating);
-};
-
-const find = (query, by) => {
-	MovieDataService.find(query, by)
-		.then((response) => {
-			console.log(response.data);
-			setMovies(response.data.movies);
-		})
-		.catch((e) => {
-			console.log(e);
-		});
-};
-
-const findByTitle = () => {
-	find(searchTitle, "title");
-};
-
-const findByRating = () => {
-	if (searchRating === "All Ratings") {
+	useEffect(() => {
 		retrieveMovies();
-	} else {
-		find(searchRaing, "rated");
-	}
-	return (
-		<div className="App">
-			<Container>
-				<Form>
-					<Row>
-						<Col>
-							<Form.Group>
-								<Form.Control
-									type="text"
-									placeholder="Search by title"
-									value="{searchTitle}
-						onChange={onChangeSearchTitle}"
-								></Form.Control>
-							</Form.Group>
-							<Button variant="primary" type="button" onClick={findByTitle}>
-								Search
-							</Button>
-						</Col>
-						<Col>
-							<Form.Group>
-								<Form.Control as="select" onChange={onChangeSearchRating}>
-									{" "}
-									{rating.map((rating) => {
-										return <option value={rating}> {rating}</option>;
-									})}
-								</Form.Control>
-							</Form.Group>
-							<Button variant="primary" type="button" onClick={findByRating}>
-								Search
-							</Button>
-						</Col>
-					</Row>
-				</Form>
-				<Row>
-					{movies.map((movie) => {
-						return (
+		retrieveRatings();
+	}, []);
+
+	const retrieveMovies = () => {
+		MovieDataService.getAll()
+			.then((response) => {
+				console.log(response.data);
+				setMovies(response.data.movies);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
+
+	const retrieveRatings = () => {
+		MovieDataService.getRatings()
+			.then((response) => {
+				console.log(response.data);
+				setRatings(["All Ratings"].concat(response.data));
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
+
+	const onChangeSearchTitle = (e) => {
+		const searchTitle = e.target.value;
+		setSearchTitle(searchTitle);
+	};
+
+	const onChangeSearchRating = (e) => {
+		const searchRating = e.target.valuesetSearchRating(searchRating);
+	};
+
+	const find = (query, by) => {
+		MovieDataService.find(query, by)
+			.then((response) => {
+				console.log(response.data);
+				setMovies(response.data.movies);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
+
+	const findByTitle = () => {
+		find(searchTitle, "title");
+	};
+
+	const findByRating = () => {
+		if (searchRating === "All Ratings") {
+			retrieveMovies();
+		} else {
+			find(searchRating, "rated");
+		}
+		return (
+			<div className="App">
+				<Container>
+					<Form>
+						<Row>
 							<Col>
-								<Card style={{ width: "18rem" }}>
-									<Card.Img src={movie.poster + "/100px180"} />
-									<Card.Body>
-										<Card.title>{movie.title}</Card.title>
-										<Card.Text>Rating: {movie.rated}</Card.Text>
-										<Link to={"/movies/" + movie._id}> View Reviews </Link>
-									</Card.Body>
-								</Card>
+								<Form.Group>
+									<Form.Control
+										type="text"
+										placeholder="Search by title"
+										value="{searchTitle}
+						onChange={onChangeSearchTitle}"
+									></Form.Control>
+								</Form.Group>
+								<Button variant="primary" type="button" onClick={findByTitle}>
+									Search
+								</Button>
 							</Col>
-						);
-					})}
-				</Row>
-			</Container>
-		</div>
-	);
+							<Col>
+								<Form.Group>
+									<Form.Control as="select" onChange={onChangeSearchRating}>
+										{" "}
+										{ratings.map((rating) => {
+											return <option value={rating}> {rating}</option>;
+										})}
+									</Form.Control>
+								</Form.Group>
+								<Button variant="primary" type="button" onClick={findByRating}>
+									Search
+								</Button>
+							</Col>
+						</Row>
+					</Form>
+					<Row>
+						{movies.map((movie) => {
+							return (
+								<Col>
+									<Card style={{ width: "18rem" }}>
+										<Card.Img src={movie.poster + "/100px180"} />
+										<Card.Body>
+											<Card.title>{movie.title}</Card.title>
+											<Card.Text>Rating: {movie.rated}</Card.Text>
+											<Link to={"/movies/" + movie._id}> View Reviews </Link>
+										</Card.Body>
+									</Card>
+								</Col>
+							);
+						})}
+					</Row>
+				</Container>
+			</div>
+		);
+	};
 };
 
-export default App;
+export default MoviesList;
